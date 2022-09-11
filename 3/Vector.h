10f0e100bf5a7b401e587158df09045c70fb1ef9
muxiongguo <1721030720@qq.com>
2022-09-11 13:14:53 +0800

    9 继续书写 Vector

--- 3/Vector.h
+++ 3/Vector.h
@@ -11,20 +11,21 @@ class Vector {
 public:
     // 五大函数
     // 0默认构造函数
-    explicit Vector(int init = 0) : size(init), capacity(init + SPARE_CAPACITY) {
-        objects = new Object[capacity];
+    explicit Vector(int init = 0) : size_(init), capacity_(init + kSPARE_CAPACITY) {
+        objects_ = new Object[capacity_];
     }
 
     // 1拷贝构造函数
-    Vector(const Vector &rhs) : size(rhs.size), capacity(rhs.capacity), objects(nullptr) {
-        objects = new Object[capacity];
-        for (int i = 0; i < size; ++i) {
-            objects[i] = rhs.objects[i];
+    Vector(const Vector &rhs) : size_(rhs.size_), capacity_(rhs.capacity_), objects_(nullptr) {
+        objects_ = new Object[capacity_];
+        for (int i = 0; i < size_; ++i) {
+            objects_[i] = rhs.objects_[i];
         }
     }
 
     // 2拷贝赋值函数
     Vector &operator=(const Vector &rhs) {
+        if (this==&rhs) return *this; // 防止自赋值
         Vector copy = rhs;
         std::swap(*this, copy);
         return *this;
@@ -32,55 +33,90 @@ public:
 
     // 3析构函数
     ~Vector() {
-        delete[] objects;
+        delete[] objects_;
     }
 
     // 4移动构造函数
-    Vector(Vector &&rhs) noexcept : size(rhs.size), capacity(rhs.capacity), objects(rhs.objects) {
-        rhs.objects = nullptr;
-        rhs.size = 0;
-        rhs.capacity = 0;
+    Vector(Vector &&rhs) noexcept : size_(rhs.size_), capacity_(rhs.capacity_), objects_(rhs.objects_) {
+        rhs.objects_ = nullptr;
+        rhs.size_ = 0;
+        rhs.capacity_ = 0;
     }
 
     // 5移动赋值函数
     Vector &operator=(Vector &&rhs) noexcept {
-        std::swap(size, rhs.size);
-        std::swap(capacity, rhs.capacity);
-        std::swap(objects, rhs.objects);
+        std::swap(size_, rhs.size_);
+        std::swap(capacity_, rhs.capacity_);
+        std::swap(objects_, rhs.objects_);
         return *this;
     }
 
     void reserve(int new_capacity){
-        if (new_capacity < capacity)
+        if (new_capacity < capacity_)
             return;
-        Object *new_array = new Object[new_capacity];
-        for (int i = 0; i < size; ++i)
-            new_array[i] = std::move(objects[k]);
-        capacity = new_capacity;
-        std::swap(objects, new_array);
+        auto *new_array = new Object[new_capacity];
+        for (int i = 0; i < size_; ++i)
+            new_array[i] = std::move(objects_[i]);
+        capacity_ = new_capacity;
+        std::swap(objects_, new_array);
         delete [] new_array;
     }
 
     void resize(int new_size){
-        if (new_size > capacity)
+        if (new_size > capacity_)
             reserve(new_size * 2);
-        size = new_size;
+        size_ = new_size;
     }
 
     // 两种[]重载 适应不同情况，const和非const，对应修改和访问
     Object &operator[](int index){
-        return objects[index];
+        return objects_[index];
     }
 
     const Object &operator[](int index) const{
-        return objects[index];
+        return objects_[index];
     }
 
-    static const int SPARE_CAPACITY = 16; //静态变量，不能在对象的构造函数中初始化
+    bool empty() const{
+        return size_ == 0;
+    }
+
+    int size() const{
+        return size_;
+    }
+
+    int capacity() const{
+        return capacity_;
+    }
+
+    void push_back(const Object &x){
+        if (size_ == capacity_)
+            reserve(2 * capacity_ + 1);
+        objects_[size_++] = x;
+    }
+
+    void push_back(Object &&x){
+        if (size_ == capacity_)
+            reserve(2 * capacity_ + 1);
+        objects_[size_++] = std::move(x);
+    }
+
+    void pop_back(){
+        --size_;
+    }
+
+    const Object &back() const{
+        return objects_[size_ - 1];
+    }
+
+    using iterator = Object *;
+    using const_iterator = const Object *;
+
+    static const int kSPARE_CAPACITY = 16; //静态变量，不能在对象的构造函数中初始化
 private:
-    int size = 0;
-    int capacity = 0;
-    Object *objects = nullptr;
+    int size_ = 0;
+    int capacity_ = 0;
+    Object *objects_ = nullptr;
 };
 
 #endif //DATASTRUCTUREALGORITHM_VECTOR_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "3/Vector.h"
+#include <vector>
+using namespace std;
+
+int main(){
+    //Vector<int> v{12,34,55};
+    Vector<int> v;
+    int a = 11;
+    v.push_back(a);
+    return 0;
+}
+
