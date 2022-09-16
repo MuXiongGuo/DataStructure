06c7a352507f4dbdff2cd15f770ae03630865034
muxiongguo <muxiongguo@gmail.com>
2022-09-16 20:54:51 +0800

    约瑟夫环   应该用链表

--- 3/3.6.cpp
+++ 3/3.6.cpp
@@ -6,21 +6,69 @@
 #include <vector>
 
 using namespace std;
-
-vector<int> JosephusSolution(int m, int n) {
-    vector<int> v1(n + 1);
-    m += 1;
-    int people_remaining = n;
-    int i = 0;
-    while (n != 0) {
-        if (i % m == 0)
-            v1[i] = 1;
-        while (v1[i] != 0) ++i;
-
-
-
+// my solution
+void JosephusSolution(int m, int n) {
+    if (m == 0){
+        for (int i = 1; i <= n; ++i) {
+            cout << i << " ";
         }
-    return v1;
+        return;
+    }
+    vector<int> v1(n + 1);
+    auto remain_people = n;
+    m += 1;
+    int i = 1;
+    int count = 1;
+    while (remain_people != 0) {
+        if (count % m == 0) {
+            v1[i] = 1;
+            --remain_people;
+            cout << i << ' ';
+            count = 1;
+        } else{
+            ++count;
+            ++i;
+            if (i > n)
+                i = 1;
+        }
+        while (remain_people != 0 && v1[i] != 0){
+            ++i;
+            if (i > n)
+                i = 1;
+        }phux
+    }
+}
+
+// assistant's solution
+void Josephus(int n, int m) {
+    vector<int> people;
+    for (int i = 0; i < n; ++i) {
+        people.push_back(i + 1);
+    }
+    int index = 0;
+    while (people.size() > 1) {
+        index = (index + m - 1) % people.size();
+        cout << people[index] << " ";
+        people.erase(people.begin() + index);
+    }
+    cout << people[0];
+}
+
+int main(){
+    JosephusSolution(2, 7);
+    cout << endl;
+    JosephusSolution(6, 4);
+    cout << endl;
+    JosephusSolution(1, 5);
+    cout << endl;
+    // 验证另一个solution
+    Josephus(2, 7);
+    cout << endl;
+    Josephus(6, 4);
+    cout << endl;
+    Josephus(1, 5);
+    cout << endl;
+    return 0;
 }
 
 
@@ -32,18 +80,3 @@ vector<int> JosephusSolution(int m, int n) {
 
 
 
-
-
-//void Josephus(int n, int m) {
-//    vector<int> people;
-//    for (int i = 0; i < n; ++i) {
-//        people.push_back(i + 1);
-//    }
-//    int index = 0;
-//    while (people.size() > 1) {
-//        index = (index + m - 1) % people.size();
-//        cout << people[index] << " ";
-//        people.erase(people.begin() + index);
-//    }
-//    cout << people[0];
-//}
