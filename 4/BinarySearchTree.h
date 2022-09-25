9ceb349b646ae633ae867767f685572608661742
muxiongguo <muxiongguo@gmail.com>
2022-09-25 21:10:19 +0800

    Tree 写了 findmax findmin insert remove 等操作 顺便复习一下 const对象不能std::move

--- 4/BinarySearchTree.h
+++ 4/BinarySearchTree.h
@@ -60,13 +60,74 @@ private:
 
     BinaryNode *root;
 
-    void Insert(const Comparable &x, BinaryNode *&t);
-    void Insert(Comparable &&x, BinaryNode *&t);
-    void Remove(const Comparable &x, BinaryNode *&t);
-    BinaryNode *FindMin(BinaryNode *t) const;
-    BinaryNode *FindMax(BinaryNode *t) const;
+    void Insert(const Comparable &x, BinaryNode *&t){
+        if (t == nullptr)
+            t = new BinaryNode(x, nullptr, nullptr);
+        else if (t->element > x)
+            Insert(x, t->left);
+        else
+            Insert(x, t->right);
+        // 不处理重复元
+    }
+    void Insert(Comparable &&x, BinaryNode *&t){
+        if (t == nullptr)
+            t = new BinaryNode(std::move(x), nullptr, nullptr);
+        else if (t->element > x)
+            Insert(std::move(x), t->left);
+        else
+            Insert(std::move(x), t->right);
+        // 不处理重复元
+    }
+    void Remove(const Comparable &x, BinaryNode *&t){
+        if (t == nullptr)
+            return;
+        if (t->element > x)
+            Remove(x, t->left);
+        else if (t->element < x)
+            Remove(x, t->right);
+        else{
+            if (t->left == nullptr){
+                auto tem = t;
+                t = t->right;
+                delete(tem);
+            }
+            else if (t->right == nullptr){
+                auto tem = t;
+                t = t->left;
+                delete(tem);
+            }
+            else{
+                t->element = FindMin(t->right)->element;
+                Remove(t->element, t->right);
+            }
+        }
+    }
+    BinaryNode *FindMin(BinaryNode *t) const{
+        if (t == nullptr)
+            return nullptr;
+        else if (t->left == nullptr)
+            return t;
+        else if (t->left != nullptr)
+            return FindMin(t->left);
+    }
+    BinaryNode *FindMax(BinaryNode *t) const{
+        if (t == nullptr)
+            return nullptr;
+        while (t->right != nullptr){
+            t = t->right;
+        }
+        return t;
+    }
     bool Contains(const Comparable &x, BinaryNode *t) const{
-
+        if (t == nullptr)
+            return false;
+        // 按道理说可以把判断相等放最后，因为这一步最不可能发生
+        if (x == t->element)
+            return true;
+        else if (x < t->element)
+            return Contains(x, t->left);
+        else
+            return Contains(x, t->right);
     }
     void MakeEmpty(BinaryNode *&t);
     void PrintTree(BinaryNode *t, ostream &out) const;
