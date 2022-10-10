7bab647d116093c6b56dcd19fe80ce21d28843a5
muxiongguo <muxiongguo@gmail.com>
2022-10-10 11:45:51 +0800

    🍗 二叉堆 插入和删除

--- 6/BinaryHeap.h
+++ 6/BinaryHeap.h
@@ -14,10 +14,24 @@ public:
     bool isEmpty() const;
     const Comparable & findMin() const;
 
-    void insert(const Comparable &x);
+    void insert(const Comparable &x){
+        if (currentSize == array.size()-1)
+            array.resize(array.size()*2);
+        int hole = ++currentSize;
+        Comparable copy = x;
+        array[0] = std::move(x);
+        for (; x<array[hole/2]; hole/=2)
+            array[hole] = std::move(array[hole]);
+        array[hole] = std::move(array[0]);
+    }
     void insert(Comparable &&x);
 
-    void deleteMin();
+    void deleteMin(){
+        if (isEmpty())
+            throw UnderflowException{};
+        array[1] = std::move(array[currentSize--]);
+        percolateDown(1);
+    }
     void deleteMin(Comparable &minItem);
     void makeEmpty();
 private:
@@ -25,7 +39,20 @@ private:
     vector<Comparable> array;
 
     void buildHeap();
-    void percolateDown(int hole);
+    void percolateDown(int hole){
+        int child;
+        Comparable temp = std::move(array[hole]);
+        for (; hole*2 <= currentSize; hole = child){
+            child = hole*2;
+            if (child != currentSize && array[child+1] < array[child])
+                ++child;
+            if(array[child] < temp)
+                array[hole] = std::move(array[child]);
+            else
+                break;
+        }
+        array[hole] = std::move(temp);
+    }
 };
 
 
