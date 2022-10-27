e9e42f0732983a1031ef826a4ce580738038d87c
muxiongguo <muxiongguo@gmail.com>
2022-10-27 10:18:00 +0800

    🍈 字符串题目  KMP 算法

--- leetcode/28.cpp
+++ leetcode/28.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// 暴力枚举
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -22,6 +23,44 @@ public:
         return -1;
     }
 };
+
+// KMP算法
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0) {
+            return 0;
+        }
+        vector<int> pi(m);
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle[i] != needle[j]) {
+                j = pi[j - 1];
+            }
+            if (needle[i] == needle[j]) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = pi[j - 1];
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+};
+//
+//作者：LeetCode-Solution
+//        链接：https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/solution/shi-xian-strstr-by-leetcode-solution-ds6y/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 int main() {
