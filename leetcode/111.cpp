05959f8b524e4672a05500aa98ab100046955cd4
muxiongguo <muxiongguo@gmail.com>
2022-10-31 22:26:16 +0800

    🍄 树

--- leetcode/111.cpp
+++ leetcode/111.cpp
@@ -14,6 +14,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// dfs
 class Solution {
 public:
     int minDepth(TreeNode* root) {
@@ -33,3 +34,37 @@ public:
         return min(left, right) + 1;
     }
 };
+
+// bfs
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        queue<pair<TreeNode *, int> > que;
+        que.emplace(root, 1);
+        while (!que.empty()) {
+            TreeNode *node = que.front().first;
+            int depth = que.front().second;
+            que.pop();
+            if (node->left == nullptr && node->right == nullptr) {
+                return depth;
+            }
+            if (node->left != nullptr) {
+                que.emplace(node->left, depth + 1);
+            }
+            if (node->right != nullptr) {
+                que.emplace(node->right, depth + 1);
+            }
+        }
+
+        return 0;
+    }
+};
+
+作者：LeetCode-Solution
+        链接：https://leetcode.cn/problems/minimum-depth-of-binary-tree/solution/er-cha-shu-de-zui-xiao-shen-du-by-leetcode-solutio/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
