ca830646401307a2c1a8d50592c620eef03a92ec
muxiongguo <muxiongguo@gmail.com>
2022-11-11 11:58:40 +0800

    🍥 回溯

--- leetcode/77.cpp
+++ leetcode/77.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> res;
+    vector<int> path;
 
+    void BackTracking(int n, int k, int start_index) {
+        if (path.size() == k) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = start_index; i <= n - (k - path.size()) + 1; ++i) {
+            path.push_back(i);
+            BackTracking(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        BackTracking(n, k, 1);
+        return res;
     }
 };
