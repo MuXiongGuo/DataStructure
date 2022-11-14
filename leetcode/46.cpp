8e8ebcc8684a76599b38266741f56c3684dee1ed
muxiongguo <1721030720@qq.com>
2022-11-14 16:24:42 +0800

    🍥 回溯

---
+++ leetcode/46.cpp
@@ -0,0 +1,17 @@
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    void BackTracking(vector<int> &nums, int start_index) {
+
+    }
+
+    vector<vector<int>> permute(vector<int> &nums) {
+
+    }
+};
