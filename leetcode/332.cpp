adffbf4d43e3e45dec03dfac99d317c460ff009c
muxiongguo <muxiongguo@gmail.com>
2022-11-15 21:50:28 +0800

    🍥 回溯 1.难点是 容器选择  另一种是 unordered_map<出发机场, multiset<到达机场>> targets 但是不能快速删除 2.不存在的key会导致被添加进去, 但是这个key对应一个空的map

--- leetcode/332.cpp
+++ leetcode/332.cpp
@@ -2,57 +2,69 @@
 #include "vector"
 #include "string"
 #include "unordered_map"
+#include "map"
 
 using namespace std;
 
+// 难点是 容器选择  另一种是 unordered_map<出发机场, multiset<到达机场>> targets 但是不能快速删除
+// 而map的可以用 懒惰删除
 class Solution {
 public:
-    unordered_map<string, vector<string>> lookup;
+    // unordered_map<出发机场, map<到达机场, 航班次数>> targets
+    unordered_map<string, map<string, int>> targets;
 
-    void CreatLookUp(const vector<vector<string>> &tickets) {
-        for (const auto &a:tickets) {
-            lookup[a[0]].push_back(a[1]);
+    // 用bool因为只要找一个就够了 先被找到的一定是字典序的
+    // 每次只要读取res的末尾即可 不需要传递额外参数
+    bool BackTracking(vector<string> &res, int nums) {
+        if (res.size() == nums+1) {
+            return true;
         }
-    }
-
-    string FindTickets(vector<string> &tem) {
-        int res = 0;
-        for (int i = 1; i < tem.size(); ++i) {
-            if (tem[res] > tem[i])
-                res = i;
+        for(auto &target:targets[res.back()]) {
+            if (target.second !=0 ) {
+                --target.second;
+                res.emplace_back(target.first);
+                if (BackTracking(res, nums)) {return true;}
+                ++target.second;
+                res.pop_back();
+            }
         }
-        string str = tem[res];
-        tem.erase(tem.begin()+res);
-        return str;
+        return false;
     }
 
     vector<string> findItinerary(vector<vector<string>> &tickets) {
-        CreatLookUp(tickets);
         vector<string> res;
-        string start = "JFK";
-        res.emplace_back(start);
-        int count = tickets.size();
-        while (count) {
-            --count;
-            auto &tem = lookup[start];
-            start = FindTickets(tem);
-            res.emplace_back(start);
+        for (const auto &ticket: tickets) {
+            ++targets[ticket[0]][ticket[1]];
         }
+        res.emplace_back("JFK");
+        BackTracking(res, int(tickets.size()));
         return res;
     }
 };
 
 int main() {
-    // [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
-    vector<vector<string>> tickets = {{"JFK", "SFO"},
-                                      {"JFK", "ATL"},
-                                      {"SFO", "ATL"},
-                                      {"ATL", "JFK"},
-                                      {"ATL", "SFO"}};
     Solution s;
+    vector<vector<string>> tickets = {{"JFK", "KUL"}, {"JFK", "NRT"}, {"NRT", "JFK"}};
     auto res = s.findItinerary(tickets);
-    for (const auto &a:res) {
-        cout << a << " ";
+    for (const auto &str:res) {
+        cout << str << " ";
     }
+    cout << endl;
     return 0;
 }
+
+//int main() {
+//    unordered_map<string, map<string, int>>res;
+//    res["abc"]["dd"] = 18;
+//    // 不存在的key会导致被添加进去
+//    for (auto &a:res["cc"]) {
+//        cout << "hello";
+//    }
+//    // 但是这个key对应一个空的map, 相当于去遍历空集合 并不会意外访问!
+//    for (auto &a:res["cc"]) {
+//        cout << (a.second != 0);
+//    }
+//    unordered_map<string, int>lookup;
+//    lookup["dd"];
+//    return 0;
+//}
