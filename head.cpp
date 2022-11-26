c5df492631b72137314eccf1f4b7bcde9f034e0e
muxiongguo <1721030720@qq.com>
2022-11-27 00:02:20 +0800

    🏆 竞赛提速 头文件

---
+++ head.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#ifdef xay5421
+#define D(...) fprintf(stderr,__VA_ARGS__)
+#define DD(...) D(#__VA_ARGS__ "="),debug_helper::debug(__VA_ARGS__),D("\n")
+#include"/home/xay5421/debug.hpp"
+#else
+#define D(...) ((void)0)
+#define DD(...) ((void)0)
+#endif
+#define pb push_back
+#define eb emplace_back
+#define SZ(x) ((int)(x).size())
+#define each(x,v) for(auto&x:v)
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+using namespace std;
+using LL=long long;
+using ULL=unsigned long long;
