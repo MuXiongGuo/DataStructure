a1233021df29e81e043aa02ed6e31289873ddbd1
muxiongguo <muxiongguo@gmail.com>
2022-11-26 18:20:46 +0800

    🎯 dp

---
+++ leetcode/509.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        int a = 1, b = 1;
+        for (int i = 0; i < n-3; ++i) {
+            int tem = b;
+            b = a;
+            a += tem;
+        }
+        return a+b;
+    }
+};
