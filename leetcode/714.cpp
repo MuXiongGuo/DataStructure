b63b0e93e854e52879214a6e4c37542d255fb749
muxiongguo <1721030720@qq.com>
2022-11-26 23:58:37 +0800

    🍡 贪心 dp

--- leetcode/714.cpp
+++ leetcode/714.cpp
@@ -33,3 +33,28 @@ public:
         return sell;
     }
 };
+
+// 贪心  假买假卖 很难想到
+class Solution3 {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int result = 0;
+        int minPrice = prices[0]; // 记录最低价格
+        for (int i = 1; i < prices.size(); i++) {
+            // 情况二：相当于买入
+            if (prices[i] < minPrice) minPrice = prices[i];
+
+            // 情况三：保持原有状态（因为此时买则不便宜，卖则亏本）
+            if (prices[i] >= minPrice && prices[i] <= minPrice + fee) {
+                continue;
+            }
+
+            // 计算利润，可能有多次计算利润，最后一次计算利润才是真正意义的卖出
+            if (prices[i] > minPrice + fee) {
+                result += prices[i] - minPrice - fee;
+                minPrice = prices[i] - fee; // 情况一，这一步很关键
+            }
+        }
+        return result;
+    }
+};
