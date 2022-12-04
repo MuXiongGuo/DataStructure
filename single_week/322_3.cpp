569fa15a0a44b9adf3e6885073995b05618919cf
muxiongguo <1721030720@qq.com>
2022-12-04 19:55:38 +0800

    🏅🌈 周赛 图的dfs标准模版

--- single_week/322_3.cpp
+++ single_week/322_3.cpp
@@ -34,6 +34,7 @@ public:
     }
 };
 
+// 测试每条边 会造成三次方节点的 复杂度 所以很笨蛋
 class Solution2 {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
@@ -82,7 +83,7 @@ public:
         int res = INT_MAX;
         function<void(int)> dfs = [&](int u) {
             visited[u] = 1;
-            for (auto [v, weight]: adjacency_list[u]) {
+            for (auto &[v, weight]: adjacency_list[u]) {
                 if (visited[v] == 0) {
                     dfs(v);
                 }
@@ -97,6 +98,38 @@ public:
         return res;
     }
 };
+
+
+
+// 正规做法 dfs 模版
+class Solution4 {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> ad_list(n+1);
+        for (const auto &road:roads) {
+            int u = road[0], v = road[1], count = road[2];
+            ad_list[u].emplace_back(v, count);
+            ad_list[v].emplace_back(u, count);
+        }
+
+        vector<int> visited(n+1);
+        int res = INT_MAX;
+        // 定义函数对象
+        function<void(int)> dfs = [&] (int start) -> void {
+            visited[start] = 1;
+            for (const auto &[v, el]: ad_list[start]) {
+                // 先取边 再判断细节
+                res = min(res, el);
+                if (visited[v] == 0) {
+                    dfs(v);
+                }
+            }
+        };
+        dfs(1);
+        return res;
+    }
+};
+
 int main() {
 
 
