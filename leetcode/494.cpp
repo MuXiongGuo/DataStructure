f3e07c6e87b39a3aafb3bf2e92b3495b1853d98e
muxiongguo <muxiongguo@gmail.com>
2022-12-07 15:53:13 +0800

    🌈 回溯???

--- leetcode/494.cpp
+++ leetcode/494.cpp
@@ -1,3 +1,30 @@
-//
-// Created by dell on 2022/12/7.
-//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// 超时了
+class Solution {
+public:
+    int findTargetSumWays(vector<int> &nums, int target) {
+        int n = nums.size(), s = (1 << n)-1;
+        int res = 0, sum = 0;
+        for (int i = 0; i <= s; ++i) {
+           for (int j = 0; j < n; ++j) {
+               int flag = (i>>j & 1)?1:-1;
+               sum += flag*nums[j];
+           }
+           if (sum == target) {
+               ++res;
+           }
+           sum = 0;
+        }
+        return res;
+    }
+};
+int main() {
+    vector<int> nums = {1, 1, 1, 1, 1};
+    int target = 3;
+    Solution s;
+    cout << s.findTargetSumWays(nums, target) << endl;
+    return 0;
+}
