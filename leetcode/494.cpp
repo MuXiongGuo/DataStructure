b95a2d6ddc423d6964a7ffbaa595b265e684b843
muxiongguo <1721030720@qq.com>
2022-12-08 13:41:55 +0800

    🌈 01背包 加强版

--- leetcode/494.cpp
+++ leetcode/494.cpp
@@ -21,6 +21,26 @@ public:
         return res;
     }
 };
+
+// 重要的是 能想到转化为背包问题
+// 取+-号状态本质 可以转化成石头问题
+class Solution2 {
+public:
+    int findTargetSumWays(vector<int> &nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if ((sum+target)%2) {return 0;} // 一定凑不出来
+        int m = (sum+target)/2;
+        vector<int> dp(m+1);
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = m; j >= 0; --j) {
+                if (j >= nums[i]) {
+                    dp[j] = max(dp[j], dp[j-nums[i]]+nums[i]);
+                }
+            }
+        }
+
+    }
+};
 int main() {
     vector<int> nums = {1, 1, 1, 1, 1};
     int target = 3;
