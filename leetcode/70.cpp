aa874fc76b4b6cd9be4883735a350b0cbf33dcb7
muxiongguo <1721030720@qq.com>
2022-12-16 16:54:52 +0800

    🎯 完全背包解决爬楼梯

--- leetcode/70.cpp
+++ leetcode/70.cpp
@@ -36,3 +36,21 @@ public:
         return r;
     }
 };
+
+// 转换成完全背包问题
+// 而且是 完全背包的排列问题
+class Solution3 {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= 2; ++j) {
+                if (i >= j) {
+                    dp[i] += dp[i-j];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
