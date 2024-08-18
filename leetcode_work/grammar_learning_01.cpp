ab084a99be3269d17ae79f6d1c41acb07ca9d2cf
MacMuxiongGuo <muxiongguo@gmail.com>
2024-08-18 19:48:57 +0800

    语法

--- leetcode_work/grammar_learning_01.cpp
+++ leetcode_work/grammar_learning_01.cpp
@@ -1,21 +1,39 @@
 #include <cmath>
+#include <cstdio>
+
 #include "iostream"
 using namespace std;
 
 int main() {
-    int ans[1000], count = 0;
-    for (int i = 0; i <= 999; i++) {
-        int x = 0, y = i;
-        while (y != 0) {
-            x += pow((y%10),3);
-            y /= 10;
-        }
-        if (x == i) {
-            ans[count++] = i;
-        }
-    }
+    // int ans[1000], count = 0;
+    // for (int i = 0; i <= 999; i++) {
+    //     int x = 0, y = i;
+    //     while (y != 0) {
+    //         x += pow((y%10),3);
+    //         y /= 10;
+    //     }
+    //     if (x == i) {
+    //         ans[count++] = i;
+    //     }
+    // }
 
-    while (count > 0) {
-        cout << ans[--count] << '\n';
-    }
+    // while (count > 0) {
+    //     cout << ans[--count] << '\n';
+    // }
+
+    // 浮点数上溢出
+    float tooBig = 3.4e38f * 3003.0f;
+    printf("%e\n", tooBig);
+
+    float a, b;
+    a = 2.0e15 + 1.0;
+    b = a - 2.0e15;
+    auto c = '\a';
+    // printf("%e", a);
+    auto aa = 66.2 - 'A';
+    cout << sizeof(double) << endl;
+
+    // 输出蜂鸣音
+    printf("\a");
+
 }
